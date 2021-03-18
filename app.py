47bdbf4bc484638378a16af3916803dfb5295ada
ValentineLgd <valentinelagand@gmail.com>
2021-03-18 16:00:46 +0000

    Create manage_brands.html and add_brands.html pages, add brand to the catalogue functionality added to the website

--- app.py
+++ app.py
@@ -29,6 +29,29 @@ def get_brands():
     return render_template("get_brands.html", brands=brands)
 
 
+
+@app.route("/manage_brands")
+def manage_brands():
+    return render_template("manage_brands.html")
+
+
+
+@app.route("/add_brands", methods=["GET", "POST"])
+def add_brands():
+    if request.method == "POST":
+        brand = {
+            "country": request.form.get("country"),
+            "brand_name": request.form.get("brand_name"),
+            "description": request.form.get("description"),
+            "website": request.form.get("website"),
+        }
+        mongo.db.brands.insert_one(brand)
+        flash("Brand Successfully Added")
+        return redirect(url_for("get_brands"))
+    return render_template("add_brands.html")
+
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
