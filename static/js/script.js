657813833d9ba1c829532c0ffbd2b86f2915b634
ValentineLgd <valentinelagand@gmail.com>
2021-03-26 08:57:18 +0000

    Add flash messages as bootstrap modals

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+/* Code from : https://stackoverflow.com/questions/35965321/flashing-message-in-flask-on-a-bootstrap-modal */
+
+
+$(document).ready(function() {
+    var messages = "{{ get_flashed_messages() }}";
+
+    if (typeof messages != 'undefined' && messages != '[]') {
+        $("#myModal").modal();
+    };
+});
